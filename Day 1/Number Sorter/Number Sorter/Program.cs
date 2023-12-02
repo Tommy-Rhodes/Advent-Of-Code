6250777bae972f16446ea959d0358f1aed1995aa
Tom Rhodes <62999268+Tommy-Rhodes@users.noreply.github.com>
2023-12-02 10:46:14 +0000

    Add files via upload
    Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
    
    You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
    
    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
    
    You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
    
    As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
    
    The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
    
    For example:
    
    1abc2
    pqr3stu8vwx
    a1b2c3d4e5f
    treb7uchet
    In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
    
    Consider your entire calibration document. What is the sum of all of the calibration values?

---
+++ Day 1/Number Sorter/Number Sorter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int totalNumber = 0; // creates the variable to store the result
+        string[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }; // array of numbers to check for
+        string file = @"D:\Code Stuffs\Advent of Code\Day 1\numbersText.txt"; // location of the file with the puzzle
+
+        using (StreamReader read = new StreamReader(file)) // reads the file
+        {
+            string line;
+            while ((line = read.ReadLine()) != null) // loops through each line in the file
+            {
+                string digit = ""; // declares the digit variable to combine the two numbers in each line
+                string lineReversed = ""; // declares lineReversed outside the loop
+
+                foreach (char c in line) // loops through each character in the line
+                {
+                    if (Array.Exists(numbers, digit => digit == c.ToString())) // checks if the character is contained in the numbers array
+                    {
+                        Console.WriteLine("Found a digit: " + c); // if a number is found it will declare it 
+                        digit = digit + c; // adds the declared variable to the digit string
+                        char[] charArray = line.ToCharArray(); // turns the line into an array so it can be reversed
+                        Array.Reverse(charArray); // reverses the string
+                        lineReversed = new string(charArray); // stores the reversed string in a variable called lineReversed
+                        Console.WriteLine(lineReversed);
+                        break; // exits the loop so if there are more than 2 digits it won't count all of them
+                    }
+                }
+
+                foreach (char c in lineReversed) // loops through each character in the reversed line
+                {
+                    if (Array.Exists(numbers, digit => digit == c.ToString())) // checks if the character is contained in the numbers array
+                    {
+                        Console.WriteLine("Found a digit: " + c);
+                        digit = digit + c;
+                        break; // exits the loop so if there are more than 2 digits it won't count all of them
+                    }
+                }
+
+                totalNumber = totalNumber + Convert.ToInt32(digit);
+            }
+            Console.WriteLine($"The answer is {totalNumber}");
+        }
+    }
+}
+
+
+
